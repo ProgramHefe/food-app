49daa5ced4b69dedf06919c61efdb25ade208dc5
ProgramHefe <h.kosonen91@hotmail.com>
2022-08-11 19:51:07 +0300

    cart logic improved

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useContext } from "react";
+
+import CartItem from './CartItem'
 import CartContext from "../../store/card-context";
 import Modal from '../UI/Modal'
 import classes from './Cart.module.css'
@@ -9,11 +11,22 @@ const Cart = props => {
 
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
     const hasItems = cartCtx.items.length > 0;
-    
+
+    const cartItemRemoveHandler = id => {};
+
+    const cartItemAddHandler = id => { };
+
     const cartItems = ( 
     <ul className={classes['cart-items']}>
         {cartCtx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem 
+        key={item.id} 
+        name={item.name} 
+        amount={item.amount} 
+        price={item.price} 
+        onRemove = {cartItemRemoveHandler.bind(null, item.id)}
+        onAdd={cartItemAddHandler.bind(null, item)}
+        />
         ))}
 </ul>
 );

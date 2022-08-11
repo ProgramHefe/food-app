55bab8838c155c6fae8f0fd4f8983979428facc2
ProgramHefe <h.kosonen91@hotmail.com>
2022-08-11 12:27:24 +0300

    Cart button added and imported to header component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import React, { Fragment } from "react";
-import Meals from '../../assets/meals.jpg'
-import classes from './Header.module.css'
+import HeaderCartButton from "./HeaderCartButton";
+import Meals from '../../assets/meals.jpg';
+import classes from './Header.module.css';
 
 const Header = props => {
     return <Fragment>
         <header className={classes.header}>
             <h1>Meals please</h1>
-            <button>Cart</button>
+            <HeaderCartButton />
         </header>
         <div className={classes['main-image']}>
             <img src={Meals} alt='Good yummy food'/>

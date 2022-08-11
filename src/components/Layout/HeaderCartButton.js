094571dfa4ca20c0542c3f1a88f1c88c3ffe531f
ProgramHefe <h.kosonen91@hotmail.com>
2022-08-11 20:30:59 +0300

    Effect added and little additions

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,38 @@
 import React from "react";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from '../Cart/CartIcon';
 import CartContext from "../../store/card-context";
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
+   const [btnHighlighted, setBtnHighlighted] = useState(false);
    const cartCtx = useContext(CartContext);
+
+    const { items } = cartCtx;
 
    const nrOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
    }, 0);
 
-    return <button className={classes.button} onClick={props.onClick}>
+
+   const btnClasses = `${classes.button} ${btnHighlighted ? classes.bump : ''}`
+
+   useEffect(()=> {
+    if(items.lenght === 0){
+        return;
+    }
+    setBtnHighlighted(true);
+
+    const timer = setTimeout(() => {
+        setBtnHighlighted(false);
+    }, 300);
+
+    return () => {
+        clearTimeout(timer);
+    }
+   }, [items]);
+
+    return <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>

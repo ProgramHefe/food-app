a5ea4bfdc3f1f5a96410326f18f36a0ef5d4ee37
ProgramHefe <h.kosonen91@hotmail.com>
2022-08-11 19:22:22 +0300

    Cart changes

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,6 +8,7 @@ const Cart = props => {
     const cartCtx = useContext(CartContext);
 
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
+    const hasItems = cartCtx.items.length > 0;
     
     const cartItems = ( 
     <ul className={classes['cart-items']}>
@@ -26,7 +27,7 @@ return (
         </div>
         <div className={classes.actions}>
             <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-            <button className={classes.button}>Order</button>
+            {hasItems && <button className={classes.button}>Order</button>}
         </div>
     </Modal>
 )
